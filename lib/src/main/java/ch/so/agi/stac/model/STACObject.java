dfc9795ff356247de5cd00733fc26bf6e5fe0335
Stefan Ziegler <edi.gonzales@gmail.com>
2023-04-21 17:41:58 +0200

    wip

--- lib/src/main/java/ch/so/agi/stac/model/STACObject.java
+++ lib/src/main/java/ch/so/agi/stac/model/STACObject.java
@@ -12,9 +12,19 @@ public class STACObject {
         links.add(link);
     }
     
+    public void addLinks(List<Link> links) {
+        for (Link link : links) {
+            this.addLink(link);
+        }
+    }
+    
     public void removeLinks(RelType rel) {
         links.removeIf(l -> l.getRel().equals(rel));
     }
+    
+    // remove_hierarchical_links: link.isHierachical?
+    
+    // get_single_link
     
     public List<Link> getLinks(RelType rel) {
         return links.stream()
@@ -25,4 +35,29 @@ public class STACObject {
     public List<Link> getLinks() {
         return links;
     }
+    
+    // clear_links
+    
+    // get_root_link
+    
+    // property: self_href
+    
+    // get_self_href
+    
+    // set_self_href
+    
+    // get_root
+    
+    // set_root
+    
+    // get_parent
+    
+    // set_parent
+    
+    // get_stac_object
+    
+    // save_object
+    
+    // resolve_links
+    
 }
